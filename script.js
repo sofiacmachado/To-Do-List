d1ef52ab59ea620d3a35be4075d8cb7456db94bc
sofiacmachado <85806152+sofiacmachado@users.noreply.github.com>
2021-09-17 22:39:23 +0100

    Add and display button

--- script.js
+++ script.js
@@ -1,10 +1,29 @@
 $(document).ready(function () {
 
-  /* Connecting to the API server */
+  /* display task */
+  
+  let displayTask = function() {
+    $.ajax({
+      type: 'GET',
+      url:'https://altcademy-to-do-list-api.herokuapp.com/tasks?api_key=1',
+      dataType:'json',
+      success: function (response, textStatus) {
+        $('#todo-list').empty();
+        response.tasks.forEach(function (task) {
+          $('#todo-list').append('<p>' + task.content + '</p>');
+        })
+      },
+      error: function (request, textStatus, errorMessage) {
+        console.log(errorMessage);
+      }
+    })
+  }
+
+  /* create task */
   let createTask = function() {
     $.ajax({
       type: 'POST',
-      url: 'https://altcademy-to-do-list-api.herokuapp.com/tasks?api_key=140',
+      url: 'https://altcademy-to-do-list-api.herokuapp.com/tasks?api_key=1',
       contentType: 'application/json',
       dataType: 'json',
       data: JSON.stringify({
@@ -13,7 +32,8 @@ $(document).ready(function () {
         }
       }),
       success: function (response, textStatus) {
-        console.log(response);
+        $('#input-task').val('');
+        displayTask();
       },
       error: function(request, textStatus, errorMessage) {
         console.log(errorMessage);
@@ -26,4 +46,6 @@ $(document).ready(function () {
     createTask();
   });
 
+  displayTask();
+  
 });
