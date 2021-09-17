5a66ca17e171ef2960c97371940d5efa4748944d
sofiacmachado <85806152+sofiacmachado@users.noreply.github.com>
2021-09-17 08:49:46 +0100

    Add Button
    Add new task

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
-/* Connecting to the API server */
+$(document).ready(function () {
 
-$.ajax({
-  type: 'GET',
-  url: 'https://altcademy-to-do-list-api.herokuapp.com/tasks?api_key=140',
-  dataType: 'json',
-  success: function (response, textStatus) {
-    console.log(response);
-  },
-  error: function(request, textStatus, errorMessage) {
-    console.log(errorMessage);
+  /* Connecting to the API server */
+  let createTask = function() {
+    $.ajax({
+      type: 'POST',
+      url: 'https://altcademy-to-do-list-api.herokuapp.com/tasks?api_key=140',
+      contentType: 'application/json',
+      dataType: 'json',
+      data: JSON.stringify({
+        task: {
+          content: $('#input-task').val()
+        }
+      }),
+      success: function (response, textStatus) {
+        console.log(response);
+      },
+      error: function(request, textStatus, errorMessage) {
+        console.log(errorMessage);
+      }
+    });
   }
+
+ $('#input-form').on('submit', function(e) {
+    e.preventDefault();
+    createTask();
+  });
+
 });
