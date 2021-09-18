ff35cb6cb8713c772270f5e24ec7614b4a2e6375
sofiacmachado <85806152+sofiacmachado@users.noreply.github.com>
2021-09-18 23:49:41 +0100

    Update script.js

--- script.js
+++ script.js
@@ -5,11 +5,12 @@ $(document).ready(function () {
   let displayTask = function() {
     $.ajax({
       type: 'GET',
-      url:'https://altcademy-to-do-list-api.herokuapp.com/tasks?api_key=1',
+      url:'https://altcademy-to-do-list-api.herokuapp.com/tasks?api_key=140',
       dataType:'json',
       success: function (response, textStatus) {
         $('#todo-list').empty();
         response.tasks.forEach(function (task) {
+          console.log(response);
           $('#todo-list').append('<p>' + task.content + '</p>');
         })
       },
@@ -23,7 +24,7 @@ $(document).ready(function () {
   let createTask = function() {
     $.ajax({
       type: 'POST',
-      url: 'https://altcademy-to-do-list-api.herokuapp.com/tasks?api_key=1',
+      url: 'https://altcademy-to-do-list-api.herokuapp.com/tasks?api_key=140',
       contentType: 'application/json',
       dataType: 'json',
       data: JSON.stringify({
@@ -45,7 +46,38 @@ $(document).ready(function () {
     e.preventDefault();
     createTask();
   });
+  displayTask();
 
+  /* delete a task */
+  let deleteTask = function (id) {
+    $.ajax({
+    type:'DELETE',
+    url: 'https://altcademy-to-do-list-api.herokuapp.com/tasks/' + id + '?api_key=140',
+    success: function(response, textStatus) {
+      displayTask();
+    },
+    error: function (request, textStatus, errorMessage) {
+      console.log(errorMessage);
+    }
+  })
+}
+
+$(document).on('click', '.delete', function () {
+  deleteTask($(this).data('id'))
+});
   displayTask();
   
+ let markComplete = function () {
+   $.ajax({
+    type:'PUT',
+    url: 'https://altcademy-to-do-list-api.herokuapp.com/tasks/3669/mark_complete?api_key=140',
+    dataType: 'json',
+    success: function (response, textStatus) {
+      console.log(response);
+    },
+    error: function(request, textStatus, errorMessage) {
+      console.log(errorMessage);
+    }
+  })
+}
 });
